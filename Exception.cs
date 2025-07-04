6535f1ddb756bd90b13682e9cb44695d29d674f6
Dharshini C <dharsh20102003@gmail.com>
2025-07-04 16:30:30 +0530

    Add files via upload

---
+++ Exception.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StudentManagementSystem.Exceptions
+{
+    
+    public class StudentNotFoundException : Exception
+    {
+        public StudentNotFoundException()
+            : base("Student not found.") { }
+    }
+
+    // When invalid input is given (like age < 0 or name is empty)
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+            : base("Invalid input provided.") { }
+    }
+
+    // When a database operation fails
+    public class DatabaseException : Exception
+    {
+        public DatabaseException()
+            : base("Database operation failed.") { }
+    }
+}
